515b70b78cffbc29b6071b7ff71d642c4d3857a0
Umesh Tiruvalluru <tiruvalluruumesh@gmail.com>
2025-10-29 21:17:55 +0530

    deleted client

--- client/hooks/use-password-vsibility.ts
+++
@@ -1,8 +0,0 @@
-// hooks/usePasswordVisibility.ts
-import { useState } from "react";
-
-export function usePasswordVisibility() {
-  const [showPassword, setShowPassword] = useState(false);
-  const togglePassword = () => setShowPassword((prev) => !prev);
-  return { showPassword, togglePassword };
-}
